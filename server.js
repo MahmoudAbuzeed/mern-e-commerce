a821b47d07562b22c50c28ee3276d41c24d44411
Mahmoud Abuzeed <mahmoudabozeed56@gmail.com>
2020-08-24 14:44:32 +0200

    modification on server side and initialize client side

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const v1 = require("./src/routes/v1");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -32,6 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // ----------- Routes -----------//
+app.use("/api/v1", v1);
 
 // ----------- ERRORS -----------//
 app.use((req, res, next) => {
